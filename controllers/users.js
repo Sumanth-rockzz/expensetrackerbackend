0dcf2d0a599226b2909fc294f1239cb29d0b6ee9
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-02-18 11:57:09 +0530

    added user password encrypting algorithm (bycrypt)

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const User=require('../models/users');
 
+const bcrypt=require('bcrypt');
+
 function isStringInvalid(string){
     if(string==undefined ||string.length===0){
         return true;
@@ -18,41 +20,54 @@ exports.addUsers=async(req,res,next)=>{
         isStringInvalid(number)||isStringInvalid(password)){
 
             return res.status(400).json({message:"Bad parameters:Something is missing"})
-
         }
+    const saltrounds=10;
+    bcrypt.hash(password,saltrounds,async(err,hash)=>{
+        try{
+            await User.create({username,email,number,password:hash}) 
+        res.status(201).json({message:"Successfully Created New User"});
+        }
+        catch(err){
 
-    const data= await User.create({username,email,number,password}) 
-    res.status(201).json({message:"Successfully Created New User"});
-    }
-    catch(err){
-       if(err.name="SequelizeUniqueConstraintError")
-        {
+            console.log(err);
+        if(err.name="SequelizeUniqueConstraintError"){
            err="User Already Exists";
         } 
         else{
         err="OOPS! Something Went wrong";
         }
-
+            res.status(500).json({
+                message:err
+            });
+        }  
+    })
+    }
+    catch(err){
+        console.log("here it is ",err)
         res.status(500).json({
             message:err
         });
     }
 }
 
-exports.login   = async (req,res,next)=>{
+exports.login= async (req,res,next)=>{
     try{
-        
         const{email,password}=req.body;
-        console.log(email);
         const user= await User.findAll({where:{email:email}});
+        console.log(user);
         if(user.length>0)
         {
-            if(user[0].password===password){
+        bcrypt.compare(password,user[0].password,(err,result)=>{
+            if(err){
+              throw new Error("Something Went Wrong");
+            }
+            if(result===true){
                 return res.status(200).json({success:true,message:"User Logged in  Successfully"});
             }
-            else{
+                else{
                 return res.status(400).json({success:false,message:"Password is invalid"});
-            }
+             }
+        })   
         }
         else{
             return res.status(404).json({success:false,message:"User does Not Exist"});
